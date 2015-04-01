12fba7faca77b5e38d51ee185e3cf26f5962fd4d
Mike Joseph <mike.joseph5@gmail.com>
2015-03-31 21:38:36 -0400

    core supporter

---
+++ modl_giving_impact/libraries/api/supporter.php
@@ -0,0 +1,140 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ *
+ * @package		Giving Impact
+ * @author		Minds On Deisgn Lab
+ * @copyright	Copyright (c) 2015, Minds On Design Lab Inc.
+ * @license
+ * @link		http://givingimpact.com
+ * @since		Version 2.0
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+class Modl_API_Supporter extends Giving_impact_api {
+
+	private $api_path = 'supporters';
+
+	private $limit = 10;
+	private $offset = 0;
+	private $sort = 'created_at';
+	private $dir = 'asc';
+	private $related = false;
+
+	private $max_limit = 100;
+
+	public function process() {
+
+		if( $this->EE->TMPL->fetch_param('supporter', false) ) {
+			return $this->fetch_single();
+		}
+
+		return $this->fetch();
+	}
+
+	public function fetch_supporter($token, $rel = false) {
+
+		$url = $this->build_url($this->api_path.'/'.$token, array(
+			'status' => false,
+			'related' => $rel
+		));
+
+		$data = $this->get($url);
+
+		if( !$data || !count($data['supporter']) ) {
+			return;
+		}
+
+		if( $data['error'] ) {
+			$this->EE->output->show_user_error('general', 'Error: '.$data['message']);
+		}
+
+		return $data['supporter'];
+	}
+
+	public function fetch_single($rel = false) {
+		$token = $this->EE->TMPL->fetch_param('supporter', false);
+		$related = $this->EE->TMPL->fetch_param('related', $rel);
+
+		if( $token ) {
+			$url = $this->build_url($this->api_path.'/'.$token, array(
+				'related' => $related
+			));
+		}
+
+		$data = $this->get($url);
+
+		if( !$data || !count($data['supporters']) ) {
+			return;
+		}
+
+		if( $data['error'] ) {
+			$this->EE->output->show_user_error('general', 'Error: '.$data['message']);
+		}
+
+		return $this->prefix_tags('gi', array($data['supporters']));
+	}
+
+	public function fetch() {
+		$limit = $this->EE->TMPL->fetch_param('limit', $this->limit);
+		$offset = $this->EE->TMPL->fetch_param('offset', $this->offset);
+		$sort = str_replace(
+			'gi_', '', $this->EE->TMPL->fetch_param('sort', $this->sort)
+		);
+		$related = $this->EE->TMPL->fetch_param('related', $this->related);
+
+		if( $sort && strpos($sort, 'gi_') === 0 ) {
+			$sort = substr($sort, 3);
+		}
+
+		$dir = $this->dir;
+		if( strpos($sort, '|') !== false ) {
+			$temp = explode('|', $sort);
+			$sort = $temp[0];
+			if( count($temp) > 1 && ($temp[1] == 'desc' || $temp[1] == 'asc') ) {
+				$dir = $temp[1];
+			}
+		}
+
+		$url = $this->build_url(
+			'supporters',
+			array(
+				'limit' => $limit,
+				'offset' => $offset,
+				'sort' => sprintf('%s|%s', $sort, $dir),
+				'related' => $related,
+			)
+		);
+
+		$data = $this->get($url);
+		if( !$data || !count($data['supporters']) ) {
+			return;
+		}
+
+		if( $data['error'] ) {
+			$this->EE->output->fatal_error('Error: '.$data['message']);
+		}
+
+		return $this->prefix_tags('gi', $data['supporters'], true);
+	}
+
+	// public function post_single($data, $token = false, $related = false) {
+
+	// 	if( !$data ) {
+	// 		$this->EE->output->show_user_error('general', 'Could not encode JSON data');
+	// 	}
+
+	// 	$p = 'supporters';
+	// 	if( $token ) {
+	// 		$p .= '/'.$token;
+	// 	}
+
+	// 	$url = $this->build_url($p, array(
+	// 		'related' => $related
+	// 	));
+
+	// 	return $this->post($url, $data);
+	// }
+
+}
